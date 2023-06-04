826c5f3dacf01c8412ba42325d284661453f46e9
Vanessa Rincon <131409182+Vanery22@users.noreply.github.com>
2023-06-04 17:20:37 -0300

    Add files via upload

---
+++ js/comprar-tickets.js
@@ -0,0 +1,139 @@
+// Defino valor de ticket
+const valorTicket = 200;
+
+// const expresiones = {
+
+// 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+//  apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+// 	mail: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	
+// }
+
+// Defino porcentajes de descuento según categoría
+let descuentoEstudiante = 80;
+let descuentoTrainee    = 50;
+let descuentoJunior     = 15;
+
+// Elementos en variables
+let nombre          = document.getElementById("nombre");
+let apellido        = document.getElementById("apellido");
+let mail            = document.getElementById("mail");
+let cantidadTickets = document.getElementById("cantidadTickets");
+let categoria       = document.getElementById("categoriaSelect");
+
+// Función para quitar el estilo de error a los elementos del form
+function quitarClaseError() {
+    let x = document.querySelectorAll(".form-control, .form-select");
+    let i;
+    for (i = 0; i < x.length; i++) {
+        x[i].classList.remove('is-invalid');
+    }
+}
+
+// Cálculo total a pagar
+function total_a_pagar() {
+
+    // Ejecuto función para que quite todos los estilos de error en los campos que los tuvieran
+    quitarClaseError();
+
+    // Verifico si lleno los siguientes campos, sino que aplique un estilo de error, haga foco en el campo y se detenga
+    if (nombre.value === "") {
+        alert("Por favor, escribí tu nombre.");
+        nombre.classList.add("is-invalid");
+        nombre.focus();
+        return;
+    }
+
+    //Para determinar que el usuario no escriba números o simbolos en la celda del nombre//
+       const nombreValido = nombre => {
+      return  /^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(nombre);
+      }
+    if (!nombreValido(nombre.value)){
+        alert("Por favor ingrese sólo texto");
+        nombre.classList.add("is-invalid");
+        nombre.focus();
+        return;
+    }
+
+     //Para determinar que el usuario no escriba números o simbolos en la celda del apellido//
+    if (apellido.value === "" ) {
+        alert("Por favor, escribí tu apellido.");
+        apellido.classList.add("is-invalid");
+        apellido.focus();
+        return;
+    }
+    const apellidoValido= apellido => {
+        return  /^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(apellido)
+    }
+    if (!apellidoValido(apellido.value)){
+        alert("Por favor ingrese solo texto");
+        apellido.classList.add("is-invalid");
+        apellido.focus();
+        return;
+    }
+
+    if (mail.value === "") {
+        alert("Por favor, escribí tu email.");
+        mail.classList.add("is-invalid");
+        mail.focus();
+        return;
+    }
+
+    // Para determinar si el correo electrónico es válido o no
+    const emailValido = mail => {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(mail);
+    }
+
+    if (!emailValido(mail.value)) {
+        alert("Por favor, escribí un correo electrónico válido.");
+        mail.classList.add("is-invalid");
+        mail.focus();
+        return;
+    }
+
+    // Verifico si está ingresado al menos 1 ticket, sino que aplique un estilo de error, haga foco en el campo y se detenga
+    if ( (cantidadTickets.value == 0) || (isNaN(cantidadTickets.value)) ) {
+        alert("Por favor, ingresá correctamente cantidad de tickets.");
+        cantidadTickets.classList.add("is-invalid");
+        cantidadTickets.focus();
+        return;
+    }
+
+    // Verifico que haya seleccionado una categoría, sino que aplique un estilo de error, haga foco en el campo y se detenga
+    if (categoria.value == "") {
+        alert("Por favor, seleccioná una categoría.");
+        categoria.classList.add("is-invalid");
+        categoria.focus();
+        return;
+    }
+
+    // Multiplico cantidad de tickets por el valor
+    let totalValorTickets = (cantidadTickets.value) * valorTicket;
+
+    // Aplico descuentos según categoría
+    if (categoria.value == 0) {
+        totalValorTickets = totalValorTickets ;
+    }
+    if (categoria.value == 1) {
+        totalValorTickets = totalValorTickets - (descuentoEstudiante / 100 * totalValorTickets);
+    }
+    if (categoria.value == 2) {
+        totalValorTickets = totalValorTickets - (descuentoTrainee / 100 * totalValorTickets);
+    }
+    if (categoria.value == 3) {
+        totalValorTickets = totalValorTickets - (descuentoJunior / 100 * totalValorTickets);
+    }
+
+    // Inserto el valor en el HTML
+    totalPago.innerHTML = totalValorTickets;
+}
+
+// Botón Resumen recibe un escuchador y la funcion del cálculo
+btnResumen.addEventListener('click', total_a_pagar);
+
+// Función para el botón Borrar para que borre el valor
+function reset_total_a_pagar() {
+    quitarClaseError();
+    totalPago.innerHTML = "";
+}
+btnBorrar.addEventListener('click', reset_total_a_pagar);
